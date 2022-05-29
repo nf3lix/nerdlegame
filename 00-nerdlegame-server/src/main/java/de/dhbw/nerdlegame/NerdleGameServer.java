ebaccababc861003e368bf0d866b82a96b2c53fa
nf3lix <felix01.schneider@gmail.com>
2022-05-29 20:50:37 +0200

    Remove obsolete NerdleGame object in Server Main method

--- 00-nerdlegame-server/src/main/java/de/dhbw/nerdlegame/NerdleGameServer.java
+++ 00-nerdlegame-server/src/main/java/de/dhbw/nerdlegame/NerdleGameServer.java
@@ -6,18 +6,10 @@ import java.io.IOException;
 
 public class NerdleGameServer {
 
-    private static final String PREFIX = "[SERVER]";
-
     public static void main(String[] args) throws IOException {
-        final NerdleGame nerdleGame = new NerdleGame(new CalculationGeneratorImpl(), new GameTimerImpl());
-        nerdleGame.addGameStateChangedListener(gameState -> logMessage("GameState changed: " + gameState.name()));
         final Server server = new Server(5000);
         server.addClientConnectedListener(new GameQueue());
         server.start();
-    }
-
-    public static void logMessage(final String message) {
-        System.out.println(PREFIX + " " + message);
     }
 
 }
