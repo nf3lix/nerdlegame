179aada3a31d49d9ce160f2429276f381fbd6a9c
nf3lix <felix01.schneider@gmail.com>
2022-05-29 17:09:48 +0200

    Read random calculation from file

--- 00-nerdlegame-server/src/main/java/de/dhbw/nerdlegame/CalculationGeneratorImpl.java
+++ 00-nerdlegame-server/src/main/java/de/dhbw/nerdlegame/CalculationGeneratorImpl.java
@@ -2,11 +2,36 @@ package de.dhbw.nerdlegame;
 
 import de.dhbw.nerdlegame.calculation.Calculation;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class CalculationGeneratorImpl implements CalculationGenerator {
+
+    private final List<String> validCalculations = new ArrayList<>();
+
+    public CalculationGeneratorImpl() {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        final URL url = classLoader.getResource("valid_calculations.txt");
+        try(BufferedReader br = new BufferedReader(new FileReader(url.getPath()))) {
+            String line;
+            while((line = br.readLine()) != null) {
+                validCalculations.add(line);
+            }
+        } catch (IOException | NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public Calculation nextCalculation() {
-        return new Calculation("12+35=47"); // TODO: return random calculation
+        Random random = new Random();
+        final String calculation = validCalculations.get(random.nextInt(validCalculations.size()));
+        return new Calculation(calculation);
     }
 
 }
