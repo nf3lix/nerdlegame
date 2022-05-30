31f5156bbac7cbe75a751c7328f675079112c4c8
nf3lix <felix01.schneider@gmail.com>
2022-05-30 19:54:05 +0200

    Search game via ui

--- 00-nerdlegame-client/src/main/java/de/dhbw/nerdlegame/NerdleGameClient.java
+++ 00-nerdlegame-client/src/main/java/de/dhbw/nerdlegame/NerdleGameClient.java
@@ -7,9 +7,20 @@ import java.io.IOException;
 public class NerdleGameClient {
 
     public static void main(String[] args) throws IOException {
-        new MainWindow();
-        final ConnectionObserver observer = new ConnectionObserverImpl();
-        final Client client = new Client("127.0.0.1", 5000, observer);
+        final MainWindow window = new MainWindow();
+        window.addFindGameButtonClickListener(action -> {
+            final Runnable runnable = () -> {
+                final ConnectionObserver observer = new ConnectionObserverImpl();
+                try {
+                    final Client client = new Client("127.0.0.1", 5000, observer);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            };
+            new Thread(runnable).start();
+
+        });
+
     }
 
 }
--- 00-nerdlegame-client/src/main/java/de/dhbw/nerdlegame/ui/MainWindow.java
+++ 00-nerdlegame-client/src/main/java/de/dhbw/nerdlegame/ui/MainWindow.java
@@ -2,22 +2,28 @@ package de.dhbw.nerdlegame.ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class MainWindow {
 
+    private final JButton findGameButton = new JButton("Find game...");
     private final JFrame window = new JFrame("NerdleGame");
 
     public MainWindow() {
         window.setSize(800, 600);
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         window.setLayout(new BorderLayout());
         window.add(new CalculationField().getPanel(), BorderLayout.CENTER);
         window.add(new GameLog().getPanel(), BorderLayout.SOUTH);
         final JPanel west = new JPanel(new GridLayout(5, 1));
-        final JButton button = new JButton("Find game...");
-        west.add(button);
+        west.add(findGameButton);
         window.add(west, BorderLayout.WEST);
         window.setResizable(false);
         window.setVisible(true);
+    }
+
+    public void addFindGameButtonClickListener(final ActionListener listener) {
+        findGameButton.addActionListener(listener);
     }
 
 }
